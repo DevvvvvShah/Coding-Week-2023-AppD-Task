5e1e7a1f8899b98eaae1847bac03a5c55139f5d5
Dev Shah <dev.shah@iitg.ac.in>
2023-05-20 20:38:12 +0530

    Make necessary changes to make the app for me

--- app/src/main/java/com/example/introductionapp/MainActivity.kt
+++ app/src/main/java/com/example/introductionapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.introductionapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.WindowManager
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openBrowser(view: View) {
+        //Get url from tag
+        val url = view.tag as String
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+
+        //pass the url to intent data
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 }
